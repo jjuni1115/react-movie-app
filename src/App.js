dadff82e2ec911d1df3c0a85a8dc14840267f98c
jjuni1115 <junyege@naver.com>
2025-01-30 15:14:34 +0900

    라우터 경로 설정 변경

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ function App() {
 
 
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={`${process.env.PUBLIC_URL}`}>
             <Routes>
-                <Route path={`${process.env.PUBLIC_URL}/movie:id`} element={<Detail />} />
-                <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
+                <Route path={`/movie/:id`} element={<Detail />} />
+                <Route path={`/`} element={<Home />} />
             </Routes>
         </BrowserRouter>
     );
